2650dc8c954b041b3aa1a47c914b78a9221b8fd1
Denis Efremov <efremov@linux.com>
2018-08-24 09:44:24 +0300

    get balance

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -353,12 +353,17 @@ var Balance;
       web3 = window.web3;
       console.log(web3.version);
 
-      web3.eth.getAccounts().then(accs => { Account = accs[0] });
+      web3.eth.getAccounts().then(accs => { Account = accs[0]; console.log("Initial account: " + Account); });
+      Balance = web3.eth.getBalance(Account).then(balance => { Balance = balance; console.log("Initial balance: " + Balance); });
       setInterval(function() {
          web3.eth.getAccounts().then(accs => {
             if (accs[0] !== Account) {
                Account = accs[0];
                console.log("New account: " + Account);
+               web3.eth.getBalance(Account).then(balance => {
+                   Balance = balance;
+                   console.log("New balance: " + Balance);
+               });
             }
          });
       }, 100);
