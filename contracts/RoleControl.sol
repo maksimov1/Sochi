882715c82fd81ac212bccb1d1f0f4034e74a6b20
Denis Efremov <efremov@ispras.ru>
2018-08-27 13:06:40 +0300

    contract: add admin role

--- contracts/RoleControl.sol
+++ contracts/RoleControl.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.4.24;
 import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 
 contract RoleControl is Ownable {
-  //роли: никто, торгаш, покупатель + запросы на роли от пользователей
-  enum Role {EMPTY, TSP, CLIENT, REQ_TSP, REQ_CLIENT}
+  //роли: никто, админ, торгаш, покупатель + запросы на роли от пользователей
+  enum Role {EMPTY, ADMIN, TSP, CLIENT, REQ_TSP, REQ_CLIENT}
   mapping (address => Role) public roles;
    
   uint256 public pricePerToken;
@@ -16,9 +16,13 @@ contract RoleControl is Ownable {
   uint256 public numberOfRequests;
   
 
+  modifier onlyAdmin() {
+      require(roles[msg.sender] == Role.ADMIN);
+      _;
+  }
+
   function sendRegRequest(uint256 _phone, Role _role) public returns (uint256)
   {
-      require(msg.sender != owner);
       require(roles[msg.sender] == Role.EMPTY);
       require(isPhoneRegistered[_phone] == false);
       require(_role == Role.REQ_TSP || _role == Role.REQ_CLIENT);
@@ -32,9 +36,9 @@ contract RoleControl is Ownable {
       return numberOfRequests - 1;
   } 
 
-  function applyRegRequest(uint256 num) onlyOwner public
+  function applyRegRequest(uint256 _num) onlyAdmin public
   {
-      address applicant = requests[num];
+      address applicant = requests[_num];
       require(roles[applicant] == Role.REQ_TSP || roles[applicant] == Role.REQ_CLIENT);
 
       if (roles[applicant] == Role.REQ_TSP) {
@@ -46,9 +50,9 @@ contract RoleControl is Ownable {
       numberOfRequests--;
   }
 
-  function rejectRegRequest(uint256 num) onlyOwner public
+  function rejectRegRequest(uint256 _num) onlyAdmin public
   {
-      address applicant = requests[num];
+      address applicant = requests[_num];
       require(roles[applicant] == Role.REQ_TSP || roles[applicant] == Role.REQ_CLIENT);
 
       roles[applicant] = Role.EMPTY;
@@ -57,22 +61,40 @@ contract RoleControl is Ownable {
       numberOfRequests--;
   }
 
+  //function addAdmin() onlyOwner public
+  //Для демонстрации и тестирования onlyOwner убран
+  function addAdmin(address _addr) public
+  {
+     require(roles[_addr] == Role.EMPTY);
+     roles[_addr] = Role.ADMIN;
+  }
+
   constructor() public {
-      pricePerToken = 1;
-      minPayment = 1;
+      pricePerToken    = 1;
+      minPayment       = 1;
       numberOfRequests = 0;
+
+      roles[owner]                             = Role.ADMIN;
+      phoneByAddress[owner]                    = 74959137474;
+      isPhoneRegistered[phoneByAddress[owner]] = true;
   }
   
   function checkRole(address client) public view returns (Role) {
       return roles[client];
   }
   
-  function changePrice(uint256 newPrice) public onlyOwner {
+  // В реальности должен быть onlyOwner,
+  // но в демонстрационных целях мы разрешаем onlyAdmin
+  //function changePrice(uint256 newPrice) public onlyOwner {
+  function changePrice(uint256 newPrice) public onlyAdmin {
       require(newPrice != 0);
       pricePerToken = newPrice;
   }
-  
-  function changeMinPayment(uint256 newMinPayment) public onlyOwner {
+
+  // В реальности должен быть onlyOwner,
+  // но в демонстрационных целях мы разрешаем onlyAdmin
+  //function changeMinPayment(uint256 newMinPayment) public onlyOwner {
+  function changeMinPayment(uint256 newMinPayment) public onlyAdmin {
       require(newMinPayment != 0);
       minPayment = newMinPayment;
   }
